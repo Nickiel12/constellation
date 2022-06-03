b9c06dd5dd81a71a2c9df6e0c54510d97ce8698a
Nickiel12 <trovianflipper@gmail.com>
2022-06-02 22:20:36 -0700

    basic gtk4 template example

---
+++ src/window/imp.rs
@@ -0,0 +1,56 @@
+use glib::subclass::InitializingObject;
+use gtk::prelude::*;
+use gtk::subclass::prelude::*;
+use gtk::{glib, Button, CompositeTemplate};
+
+// Object holding the state
+// template uses magic for the path (please advise)
+#[derive(CompositeTemplate, Default)]
+#[template(resource = "/org/gtk-rs/window.ui")]
+pub struct Window {
+    #[template_child]
+    pub button: TemplateChild<Button>,
+}
+
+// The central trait for subclassing a GObject
+#[glib::object_subclass]
+impl ObjectSubclass for Window {
+    // `NAME` needs to match `class` attribute of template
+    const NAME: &'static str = "MyGtkAppWindow";
+    type Type = super::Window;
+    type ParentType = gtk::ApplicationWindow;
+
+    fn class_init(klass: &mut Self::Class) {
+        klass.bind_template();
+    }
+
+    fn instance_init(obj: &InitializingObject<Self>) {
+        obj.init_template();
+    }
+}
+
+// Trait shared by all GObjects
+// This is where the widget's button binding is
+// this class has a reference to what "self" truly is
+// beware of sentience
+impl ObjectImpl for Window {
+    fn constructed(&self, obj: &Self::Type) {
+        // Call "constructed" on parent
+        self.parent_constructed(obj);
+
+        // Connect to "clicked" signal of `button`
+        self.button.connect_clicked(move |button| {
+            // Set the label to "Hello World!" after the button has been clicked on
+            button.set_label("Hello World!");
+        });
+    }
+}
+
+// Trait shared by all widgets
+impl WidgetImpl for Window {}
+
+// Trait shared by all windows
+impl WindowImpl for Window {}
+
+// Trait shared by all application windows
+impl ApplicationWindowImpl for Window {}
